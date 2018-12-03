f34236ebf3a98ad1d63c46d4bd28588158368c24
Fun.Jim <446339431@qq.com>
2018-12-03 15:43:40 +0800

    route mime compress

---
+++ src/lib/route.js
@@ -0,0 +1,45 @@
+const fs = require('fs')
+const promisify = require('util').promisify
+const path = require('path')
+const Handlebars = require('Handlebars')
+const conf = require('../config/defaultConfig')
+const mime = require('../lib/mime')
+const compress = require('../lib/compress')
+const stat = promisify(fs.stat)
+const readdir = promisify(fs.readdir)
+
+const hbsPath = path.join(__dirname, '../template/dir.hbs')
+const template = Handlebars.compile(fs.readFileSync(hbsPath, 'utf-8'))
+
+module.exports = async function (req, res, filePath) {
+  try {
+    const stats = await stat(filePath)
+    if (stats.isFile()) {
+      res.statusCode = 200
+      const contentType = mime(filePath)
+      res.setHeader('Content-Type', contentType)
+      let rs = fs.createReadStream(filePath)
+      if (filePath.match(conf.compress)) {
+          rs = compress(rs, req, res)
+      }
+      rs.pipe(res)
+    } else if (stats.isDirectory()) {
+      const files = await readdir(filePath)
+      res.statusCode = 200
+      res.setHeader('Content-Type', 'text/html')
+
+      const dir = path.relative(conf.root, filePath)
+      const data = {
+        title: path.basename(filePath),
+        dir: dir ? `/${dir}`: '',
+        files
+      }
+      res.end(template(data))
+    }
+  } catch (error) {
+    res.statusCode = 404
+    res.setHeader('Content-Type', 'text/plain')
+    res.end(`${filePath} is not a directory ro file`)
+    return
+  }
+}
